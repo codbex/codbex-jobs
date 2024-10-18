e24ca626686105dc219449adc2446c33a7cdc4fa
TIVMOF <tomyivanov06@gmail.com>
2024-10-18 06:59:22 +0000

    update: Navigation moved to employees group

--- codbex-jobs/navigations/services/job-assignments.js
+++ codbex-jobs/navigations/services/job-assignments.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'job-assignments-navigation',
     label: "Job Assignments",
     view: "job assignments",
-    group: "configurations",
+    group: "employees",
     orderNumber: 1000,
     lazyLoad: true,
     link: "/services/web/codbex-jobs/gen/codbex-jobs/ui/JobAssignment/index.html?embedded"
