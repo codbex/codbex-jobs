e7fe9b2f897624de3097365d90e26b93e580f91f
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-12-27 11:57:32 +0200

    change perspective

--- codbex-jobs/gen/codbex-jobs/ui/JobOffer/perspective.js
+++ codbex-jobs/gen/codbex-jobs/ui/JobOffer/perspective.js
@@ -7,8 +7,8 @@ const perspectiveData = {
 	id: 'JobOffer',
 	name: 'JobOffer',
 	link: '/services/web/codbex-jobs/gen/codbex-jobs/ui/JobOffer/index.html',
-	order: 103,
-	icon: '/services/web/resources/unicons/0-plus.svg',
+	order: 119,
+	icon: '/services/web/resources/unicons/file-upload.svg',
 };
 
 if (typeof exports !== 'undefined') {
